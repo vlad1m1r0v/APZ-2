01fb3f69ed1ccc93afbac01e59a21343fa15866c
= <mypochtav@gmail.com>
2021-10-13 15:42:30 +0300

    add implementation of algorythm

--- implementation.go
+++ implementation.go
@@ -1,9 +1,53 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
 
 // TODO: document this function.
 // PrefixToPostfix converts
 func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+	var (
+		stack       []string
+		firstArg    int
+		secondArg   int
+		result      string
+		values      = strings.Fields(input)
+		value       string
+		digit, _    = regexp.Compile("[0-9]+")
+		operator, _ = regexp.Compile("[+,-,*,/,^]")
+	)
+
+	for i := len(values) - 1; i >= 0; i-- {
+		value = values[i]
+
+		if digit.MatchString(value) {
+			stack = append(stack, value)
+
+		} else if operator.MatchString(value) {
+			firstArg, _ = strconv.Atoi(stack[1])
+			secondArg, _ = strconv.Atoi(stack[0])
+
+			if value == "+" {
+				result = strconv.Itoa(firstArg + secondArg)
+			} else if value == "-" {
+				result = strconv.Itoa(firstArg - secondArg)
+			} else if value == "*" {
+				result = strconv.Itoa(firstArg * secondArg)
+			} else if value == "/" {
+				result = strconv.Itoa(firstArg / secondArg)
+			} else if value == "^" {
+				result = strconv.Itoa(int(math.Pow(float64(firstArg), float64(secondArg))))
+			}
+			stack = nil
+			stack = append(stack, result)
+		}
+
+	}
+
+	return stack[0], fmt.Errorf("TODO")
 }
