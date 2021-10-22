72c31bf356283faa527fb92c1bdde6aaa772e8a3
GVBilokorenko <abilokorenko@gmail.com>
2021-10-22 16:42:15 +0300

    rewrite tests

--- implementation_test.go
+++ implementation_test.go
@@ -2,6 +2,7 @@ package lab2
 
 import (
 	"fmt"
+	"math"
 	"testing"
 
 	. "gopkg.in/check.v1"
@@ -14,34 +15,34 @@ type TestSuite struct{}
 
 var _ = Suite(&TestSuite{})
 
-func (s *TestSuite) TestPrefixEvaluation(c *C) {
-	tests := []struct {
-		input string
-		want  string
-	}{
-		{input: "+ 2 2", want: "4"},
-		{input: "* 3 + 4 1", want: "15"},
-		{input: "^ 2 * 4 7", want: "268435456"},
-		{input: "^ 3 / 8 4", want: "9"},
-		{input: "3 * 2 * + 4 4 ^ 3 / 8 4", want: "144"},
-		{input: "* 6 + 2 * 3 + 2 ^ * + 1 3 4 5", want: "18874416"},
-		{input: "+ + 4 5", want: "invalid input"},
-		{input: "+ g 5", want: "invalid input"},
-		{input: "+ + g 5", want: "invalid input"},
-		{input: "gg", want: "invalid input"},
-		{input: "", want: "invalid input"},
-	}
+func (s *TestSuite) TestPrefixEvaluationAdd(c *C) {
+	got,_ := PrefixEvaluation("+ 2 100")
+	var want float64 = 100 + 2
+	c.Assert(got, Equals, want)
+}
 
-	for _, test := range tests {
-		got, err := PrefixEvaluation(test.input)
-		if test.want == "invalid input" {
-			c.Assert(err, Not(Equals), nil)
-			c.Assert(got, Equals, "")
-		} else {
-			c.Assert(test.want, Equals, got)
-		}
-	}
+func (s *TestSuite) TestPrefixEvaluationSub(c *C) {
+	got,_ := PrefixEvaluation("- 2 100")
+	var want float64 = 100 - 2
+	c.Assert(got, Equals, want)
+}
 
+func (s *TestSuite) TestPrefixEvaluationMul(c *C) {
+	got,_ := PrefixEvaluation("* 2 100")
+	var want float64 = 100 * 2
+	c.Assert(got, Equals, want)
+}
+
+func (s *TestSuite) TestPrefixEvaluationDiv(c *C) {
+	got,_ := PrefixEvaluation("/ 2 100")
+	var want float64 = 100 / 2
+	c.Assert(got, Equals, want)
+}
+
+func (s *TestSuite) TestPrefixEvaluationPow(c *C) {
+	got,_ := PrefixEvaluation("^ 100 2")
+	var want float64 = math.Pow(float64(100), float64(2))
+	c.Assert(got, Equals, want)
 }
 
 func ExamplePrefixEvaluation() {
